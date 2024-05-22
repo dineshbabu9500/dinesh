65817a15010c492f892154a825b79762593b4b5a
dinesh9500 <dineshbabu9500@gmail.com>
2024-05-22 12:00:24 +0530

    i done my work

---
+++ src/org/phone/Phone.java
@@ -0,0 +1,16 @@
+package org.phone;
+
+public class Phone {
+	private void phoneInfo(String name,long phno,int modelno) {
+		System.out.println("phone:"+name);
+		System.out.println("phone:"+9500718151l);
+		System.out.println("phone:"+256);
+		
+	}
+	public static void main(String[] args) {
+		Phone p = new Phone();
+		p.phoneInfo("iphone", 9500718151l, 2565);
+	}
+	
+
+}
